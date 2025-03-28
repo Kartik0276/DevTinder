ca922db289b2dc8e46fdee5eba8af3ee8ba96d70
Kartik0276 <kartikmaity60123@gmail.com>
2025-03-28 22:05:54 +0530

    playing with next()

--- src/app.js
+++ src/app.js
@@ -7,24 +7,17 @@ const app = express();
 //     res.send("HAHAHAHAHAHAHAHA....");
 // })
 
-app.get("/user/:name/:password", (req, res) => {
-    //console.log(req.query);
-    console.log(req.params);
-    res.send({
-        "FirstName" : "Kartik",
-        "LastName" : "Maity"
-    });
-})
+app.use("/user",
+    (req, res, next) => {
+        res.send("This is from first");
+        //next();
+    },
+    (req, res, next) => {
+        res.send("This is from second");
+    }
+)
 
-app.post("/user", (req, res) => {
-    //post data
-    res.send("Data is posted successfully!");
-});
 
-app.delete("/user", (req, res) => {
-    //delete data
-    res.send("The data is successfully deleted");
-})
 
 const PORT = 7777;
 app.listen(PORT, () => {
