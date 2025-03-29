19dfdb28cad7a174fac0b3b52877fd411e32ccba
Kartik0276 <kartikmaity60123@gmail.com>
2025-03-29 15:31:54 +0530

    Dynamic data signup

--- src/app.js
+++ src/app.js
@@ -3,17 +3,21 @@ const connectDB = require("./config/database")
 const app = express();
 const User = require("./models/user");
 
-app.post("/signup", async (req, res) => {
-    const userObj = {
-        firstName: "Sumana",
-        lastName: "Maity",
-        emailId: "Sumana@gmail.com",
-        password: "986543",
-        age: 23,
-        gender: "Female",
-    }
+app.use(express.json()); //This is the express middleware, it converts the json to java object so we can read the file as req.body.
 
-    const user = new User(userObj);
+app.post("/signup", async (req, res) => {
+    // const userObj = {
+    //     firstName: "Sumana",
+    //     lastName: "Maity",
+    //     emailId: "Sumana@gmail.com",
+    //     password: "986543",
+    //     age: 23,
+    //     gender: "Female",
+    // }
+
+    console.log(req.body);
+    // Creating a new instace of the User model
+    const user = new User(req.body);
     try {
         await user.save();
         res.send("New user created successfully...");
