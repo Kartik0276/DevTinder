b81a674863c4ab0a7c9db8f39d4f1dd787d1e0eb
Kartik0276 <kartikmaity60123@gmail.com>
2025-04-05 16:39:18 +0530

    Adding forgotPassword route

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,29 +1,70 @@
 const express = require('express');
 const profileRouter = express.Router();
-const { userAuth } = require("../middlewares/auth")
+const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData, forgotPassword } = require("../utils/validation");
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
-
-profileRouter.get("/profile",userAuth, async (req, res) => {
+// View profile route
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
     try {
-
-
-
-        // Validate token
-
-        //finding user based on id
         const user = req.user;
-
         res.send(user);
-
-
-
-        // console.log("Logged in user is : " + _id);
-        // console.log(decodedMessage);
-        // console.log(cookie);
-        // res.send("Cookie loading...")
     } catch (error) {
         res.status(400).send("Error: " + error.message);
     }
-})
+});
+
+// Edit profile route
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+    try {
+        if (!validateEditProfileData(req)) {
+            throw new Error("Invalid Edit Request");
+        }
+
+        const loggedInUser = req.user;
+        const updates = req.body;
+
+        Object.keys(updates).forEach(key => {
+            loggedInUser[key] = updates[key];
+        });
+
+        await loggedInUser.save();
+        res.json({
+            message: `Profile edited by ${loggedInUser.firstName}`,
+            user: loggedInUser
+        });
+    } catch (error) {
+        res.status(400).send("Error: " + error.message);
+    }
+});
+
+// Forgot password route
+profileRouter.patch("/profile/forgotPassword", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email) {
+            throw new Error("Email is required");
+        }
+
+        const user = await User.findOne({ emailId: email });
+        if (!user) {
+            throw new Error("User is not valid!!!");
+        }
+
+        if (!forgotPassword(req)) {
+            throw new Error("Please enter a strong new password...");
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+        user.password = hashPassword;
+        await user.save();
+
+        res.send("Password updated successfully...");
+    } catch (error) {
+        res.status(400).send("Error: " + error.message);
+    }
+});
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,43 +1,56 @@
 const validator = require('validator');
 
 const signupValidator = (req) => {
-    const {firstName, lastName, emailId, password, gender, age} = req.body;
+    const { firstName, lastName, emailId, password, gender, age } = req.body;
 
-    if(!firstName){
+    if (!firstName) {
         throw new Error('You must provide a first name');
     }
-    if(!lastName){
+    if (!lastName) {
         throw new Error('You must provide a last name');
     }
-    if(!emailId){
+    if (!emailId) {
         throw new Error('You must provide an email');
-    }
-    else if(!validator.isEmail(emailId)){
+    } else if (!validator.isEmail(emailId)) {
         throw new Error('Invalid email');
     }
-    if(!password){
+    if (!password) {
         throw new Error('You must provide a password');
+    } else if (password.length < 8 || password.length > 255) {
+        throw new Error('Password should be between 8 and 255 characters long');
+    } else if (!validator.isStrongPassword(password, { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 })) {
+        throw new Error("Please enter a strong password (at least 1 lowercase, 1 uppercase, 1 number, 1 symbol)");
     }
-    else if(password.length < 4 || password.length > 255){
-        throw new Error('Password should be between 4 and 255 characters long');
+    if (typeof age !== 'number' || age < 18 || age > 100) {
+        throw new Error('You must be between 18 and 100 years old');
     }
-    else if(!validator.isStrongPassword(password)){
-        throw new Error("Please enter a strong password");
-    }
-    if(!age){
-        throw new Error('You must provide your age');
-    }
-    else if(!(age >= 18)){
-        throw new Error('You must be at least 18 years old');
-    }
-    if(!gender){
+    if (!gender) {
         throw new Error('You must provide your gender');
-    }
-    else if(!(gender === 'male' || gender === 'female' || gender === 'other')){
+    } else if (!['male', 'female', 'other'].includes(gender.toLowerCase())) {
         throw new Error('Gender must be male, female or other');
     }
-}
+};
+
+const validateEditProfileData = (req) => {
+    const allowedEditFields = ["firstName", "lastName", "emailId", "photoUrl", "about", "skills", "age", "gender"];
+    const updates = Object.keys(req.body);
+    if (updates.length === 0) {
+        return false;
+    }
+    return updates.every(field => allowedEditFields.includes(field));
+};
+
+const forgotPassword = (req) => {
+    const { password } = req.body;
+
+    if (!password || typeof password !== 'string') {
+        return false;
+    }
+    return validator.isStrongPassword(password, { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 });
+};
 
 module.exports = {
-    signupValidator
-}
+    signupValidator,
+    validateEditProfileData,
+    forgotPassword,
+};
