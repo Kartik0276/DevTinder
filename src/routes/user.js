8e405525d305c2c2095173998ae46349fde529c4
Kartik0276 <kartikmaity60123@gmail.com>
2025-04-08 14:32:00 +0530

    Add pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -59,6 +59,14 @@ userRouter.get("/feed", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
+
+
+
         const userConnection = await ConnectionRequest.find({
             $or: [
                 { toUserId: loggedInUser._id },
@@ -77,9 +85,9 @@ userRouter.get("/feed", userAuth, async (req, res) => {
                 { _id: { $nin: Array.from(hideUserFromFeed) } },
                 { _id: { $ne: loggedInUser._id } }
             ]
-        }).select("firstName lastName age gender skills photoUrl about");
+        }).select("firstName lastName age gender skills photoUrl about").skip(skip).limit(limit);
 
-        res.send(users);
+        res.json({data: users});
     } catch (error) {
         res.status(400).send("Error: " + error.message);
     }
